b4d0f6e726c808adec676fb4acbebe9259a45657
Sugihara Masaki <sugihara0227@gmail.com>
2019-07-08 09:54:04 +0700

    Add Links Page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/links', to: 'static_pages#links'
   get '/signup', to: 'users#new'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -25,6 +25,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get links" do
+    get links_url
+    assert_response :success
+    assert_select "title", "Links | Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get root" do
     get root_url
     assert_response :success
