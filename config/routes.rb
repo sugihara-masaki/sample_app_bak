6319e3d6b35e6de3e54396855a9e9b6eba4f6eca
Sugihara Masaki <sugihara0227@gmail.com>
2019-07-08 17:26:42 +0700

    Add Login/Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
@@ -6,5 +8,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/links', to: 'static_pages#links'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
